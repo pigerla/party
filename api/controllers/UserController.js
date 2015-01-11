6df46b6ec18c2f2de118d268b869dd406624bc5d
ImSpy <wjj474957860@gmail.com>
2015-01-11 21:46:29 +0800

    add and update model and controller User

---
+++ api/controllers/UserController.js
@@ -0,0 +1,13 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+  hi: function (req, res) {
+    return res.send("Hi there!");
+  }
+};
+
---
+++ api/models/User.js
@@ -0,0 +1,31 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+    "username": {
+      "type": "string",
+      "defaultsTo": ""
+    },
+
+    "phoneNumber": {
+      "type": "string",
+      "defaultsTo": ""
+    },
+
+    "createdAt": {
+      "type": "datetime"
+    },
+
+    "updatedAt": {
+      "type": "datetime"
+    }
+  }
+};
+
