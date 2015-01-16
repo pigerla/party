b758403813534dcda385741654905ff21e6000ce
spy <jwu@kalengo.com>
2015-01-16 11:48:18 +0800

    update grunt coverage

--- tasks/config/mocha-istanbul.js
+++ tasks/config/mocha-istanbul.js
@@ -1,16 +1,22 @@
 /**
+ * Get coverage with mocha_istanbul
+ *
+ * ---------------------------------------------------------------
+ *
+ * For usage docs see:
+ * 		https://github.com/pocesar/grunt-mocha-istanbul
  * Created by spy on 15-1-16.
  */
 module.exports = function(grunt) {
-  grunt.config.set('mocha_istanbul', {
-    coverage: {
-      src: 'test', // the folder, not the files
-      options: {
-        coverageFolder: 'coverage',
-        mask: '**/*.test.js',
-        root: 'api/'
-      }
-    }
-  });
-  grunt.loadNpmTasks('grunt-mocha-istanbul');
+    grunt.config.set('mocha_istanbul', {
+        coverage: {
+            src: 'test', // the folder, not the files
+            options: {
+                coverageFolder: 'coverage',
+                mask: '**/*.test.js',
+                root: 'api/'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-mocha-istanbul');
 };
--- tasks/register/coverage.js
+++ tasks/register/coverage.js
@@ -3,5 +3,5 @@
  */
 
 module.exports = function (grunt) {
-  grunt.registerTask('coverage', ['mocha_istanbul:coverage']);
+    grunt.registerTask('coverage', ['mocha_istanbul:coverage']);
 };
